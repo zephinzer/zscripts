ec8fb2335c83ab91fe9c0f6ed0871bf330d472f9
zephinzer <dev@joeir.net>
2018-05-15 23:28:07 +0800

    added  `-y/--auto` to `zsc publish npm`

--- src/zsc-publish-npm.js
+++ src/zsc-publish-npm.js
@@ -13,22 +13,22 @@ commander
   .name('zsc publish npm')
   .description('publishes to npm using the git tag version as the package version')
   .option('-p, --private', 'set this flag to release the package in private mode')
+  .option('-y, --auto', 'do not confirm and just run the publish')
   .parse(process.argv);
 
-utils.versioning.list()
-  .then((tags) => {
-    const version = tags[0];
-    const packageJsonPath = path.join(process.cwd(), '/package.json');
-    const packageJson = require(packageJsonPath);
-    const tmpPackageJson = Object.assign({...packageJson}, {version});
-    inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'confirmPublish',
-        message: `Publish ${packageJson.name}@${version} to NPM?`
-      }
-    ]).then((result) => {
-      if (result.confirmPublish) {
+if (
+  commander.rawArgs.indexOf('-h') !== -1
+  || commander.rawArgs.indexOf('--help') !== -1
+) {
+  commander.help();
+} else {
+  utils.versioning.list()
+    .then((tags) => {
+      const version = tags[0];
+      const packageJsonPath = path.join(process.cwd(), '/package.json');
+      const packageJson = require(packageJsonPath);
+      const tmpPackageJson = Object.assign({...packageJson}, {version});
+      function publish() {
         log.info(`setting version property of package.json to ${version}...`);
         fs.writeFileSync(packageJsonPath, JSON.stringify(tmpPackageJson, null, 2));
         const publisher = (commander.private === true) ?
@@ -40,5 +40,20 @@ utils.versioning.list()
           process.exit(exitCode);
         });
       }
+      if (commander.auto) {
+        publish();
+      } else {
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'confirmPublish',
+            message: `Publish ${packageJson.name}@${version} to NPM?`
+          }
+        ]).then((result) => {
+          if (result.confirmPublish) {
+            publish();  
+          }
+        });
+      }
     });
-  });
+}
